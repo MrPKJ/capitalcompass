59595336cb883515b931bbb87f5942371342ad43
Priyanshukj02 <priyanshukj02@gmail.com>
2023-06-20 20:06:10 +0530

    Navbar content

---
+++ contents/navbar.ts
@@ -0,0 +1,11 @@
+import type { navDefault } from "@/types/navbar";
+
+const navbar: navDefault = {
+  defaultHome: {
+    route: "/",
+    name: "Home",
+  },
+  menuName: "More",
+};
+
+export { navbar };
