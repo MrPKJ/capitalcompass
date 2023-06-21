880ff45062057d723a230bfe95723a8cc1f6c590
Priyanshukj02 <priyanshukj02@gmail.com>
2023-06-21 11:44:13 +0530

    Navbar Established

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import "./globals.css";
+import { getWeather } from "@/utils/weather";
+import type { weather } from "@/types/weather";
+import { getCategories } from "@/utils/sanity";
+import { navbar } from "@/contents/navbar";
 import { header } from "@/contents/header";
+import type { categories } from "@/types/categories";
 
 export const metadata = {
   title: "CapitalCompass",
@@ -13,10 +18,14 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const categories: categories = await getCategories();
+  const weather: weather = await getWeather();
+
   return (
     <html lang="en">
       <body className="h-auto w-full bg-layout">
         <Header logo={header.logo} />
+        <Navbar categories={categories} weather={weather} navDefault={navbar} />
         {children}
       </body>
     </html>
