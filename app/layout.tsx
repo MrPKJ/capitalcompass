b35126409fd83eeedd5ebd240d51115aaddbb2d7
Priyanshukj02 <priyanshukj02@gmail.com>
2023-06-21 11:47:49 +0530

    Footer Established

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./globals.css";
 import { getWeather } from "@/utils/weather";
 import type { weather } from "@/types/weather";
 import { getCategories } from "@/utils/sanity";
 import { navbar } from "@/contents/navbar";
 import { header } from "@/contents/header";
+import { footer } from "@/contents/footer";
 import type { categories } from "@/types/categories";
 
 export const metadata = {
@@ -27,6 +29,14 @@ export default async function RootLayout({
         <Header logo={header.logo} />
         <Navbar categories={categories} weather={weather} navDefault={navbar} />
         {children}
+        <Footer
+          Logo={footer.Logo}
+          cta={footer.cta}
+          Socials={footer.Socials}
+          legals={footer.legals}
+          About={footer.About}
+          categories={categories}
+        />
       </body>
     </html>
   );
