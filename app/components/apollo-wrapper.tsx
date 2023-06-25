5c7de38f771b09ebe9cf9afb884051ba4d410926
Priyanshukj02 <priyanshukj02@gmail.com>
2023-06-25 13:54:05 +0530

    Apollo Wrapper Setup

---
+++ app/components/apollo-wrapper.tsx
@@ -0,0 +1,46 @@
+"use client";
+// ^ this file needs the "use client" pragma
+
+import { ApolloLink, HttpLink, SuspenseCache } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+// have a function to create a client for you
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "https://api-ap-south-1.hygraph.com/v2/clj6srgo101fi01ue2h6129bq/master",
+    fetchOptions: { cache: "no-store" },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+function makeSuspenseCache() {
+  return new SuspenseCache();
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider
+      makeClient={makeClient}
+      makeSuspenseCache={makeSuspenseCache}
+    >
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
